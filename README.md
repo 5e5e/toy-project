de666a93415d27ad07ec6b517f8ac08ec1728d7d
be4ter <be4tter@gmail.com>
2019-02-09 20:55:56 +0900

    edit gitignore

---
+++ README.md
@@ -0,0 +1,4 @@
+# toy-project
+
+## 1
+### todo-list
---
+++ todo-list/src/main/java/com/toyproject/todolist/wsh/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.toyproject.todolist.wsh;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WshApplication.class);
+	}
+
+}
+
