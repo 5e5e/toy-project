c468e60958828a9b48bf6993079abd81e9af79a7
5e5e <studenthwan@gmail.com>
2019-02-16 16:34:51 +0900

    chess pass Piece Unit Test

--- chess/src/com/practices/Object/Piece.java
+++ chess/src/com/practices/Object/Piece.java
@@ -2,22 +2,150 @@ package com.practices.Object;
 
 import com.practices.exception.WrongPieceException;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class Piece {
+	private final String blank = " ";
+	private final List<String> whitePieces = Arrays.asList("p", "n", "r", "b", "q", "k");
+	private final List<String> blackPieces = Arrays.asList("P", "N", "R", "B", "Q", "K");
+	private String color;
 	private String piece;
-
-	public Piece(String piece) {
-		if (!piece.equals(" "))
-			throw new WrongPieceException("잘못된 블랭크 요청입니다.");
-	}
 
 	public Piece() {
 
 	}
 
+	public Piece(String piece) {
+		if (!blank.equals(piece))
+			throw new WrongPieceException("잘못된 생성 요청입니다.");
+
+		this.piece = piece;
+	}
+
+	public Piece(String color, String piece) {
+		if (color.equals("white")) {
+			if (!whitePieces.contains(piece))
+				throw new WrongPieceException("하얀색 기물만 생성할 수 있습니다.");
+			this.color = color;
+			this.piece = piece;
+		} else {
+			if (!blackPieces.contains(piece))
+				throw new WrongPieceException("검은색 기물만 생성할 수 있습니다.");
+			this.color = color;
+			this.piece = piece;
+		}
+	}
+
 	public Piece makeBlank() {
 		return new Piece(" ");
+	}
+
+	public Piece makePiece(String piece) {
+		return new Piece(piece);
+	}
+
+	public Piece makeWhitePawn() {
+		return new Piece("white", "p");
+	}
+
+	public Piece makeWhitePawn(String piece) {
+		return new Piece("white", piece);
+	}
+
+	public Piece makeWhiteRook() {
+		return new Piece("white", "r");
+	}
+
+	public Piece makeWhiteRook(String piece) {
+		return new Piece("white", piece);
+
+	}
+
+	public Piece makeWhiteKnight() {
+		return new Piece("white", "n");
+	}
+
+	public Piece makeWhiteKnight(String piece) {
+		return new Piece("white", piece);
+	}
+
+	public Piece makeWhiteBishop() {
+		return new Piece("white", "b");
+	}
+
+	public Piece makeWhiteBishop(String piece) {
+		return new Piece("white", piece);
+	}
+
+	public Piece makeWhiteQueen() {
+		return new Piece("white", "q");
+	}
+
+	public Piece makeWhiteQueen(String piece) {
+		return new Piece("white", piece);
+	}
+
+	public Piece makeWhiteKing() {
+		return new Piece("white", "k");
+	}
+
+	public Piece makeWhiteKing(String piece) {
+		return new Piece("white", piece);
+	}
+
+	public Piece makeBlackPawn() {
+		return new Piece("black", "P");
+	}
+
+	public Piece makeBlackPawn(String piece) {
+		return new Piece("black", piece);
+	}
+
+	public Piece makeBlackRook() {
+		return new Piece("black", "R");
+	}
+
+	public Piece makeBlackRook(String piece) {
+		return new Piece("black", piece);
+
+	}
+
+	public Piece makeBlackKnight() {
+		return new Piece("black", "N");
+	}
+
+	public Piece makeBlackKnight(String n) {
+		return new Piece("black", piece);
+	}
+
+	public Piece makeBlackBishop() {
+		return new Piece("black", "B");
+	}
+
+	public Piece makeBlackBishop(String piece) {
+		return new Piece("black", piece);
+	}
+
+	public Piece makeBlackQueen() {
+		return new Piece("black", "Q");
+	}
+
+	public Piece makeBlackQueen(String piece) {
+		return new Piece("black", piece);
+	}
+
+	public Piece makeBlackKing() {
+		return new Piece("black", "K");
+	}
+
+	public Piece makeBlackKing(String piece) {
+		return new Piece("black", piece);
+	}
+
+	public String getPiece() {
+		return piece;
 	}
 
 	@Override
@@ -25,15 +153,21 @@ public class Piece {
 		if (this == o) return true;
 		if (!(o instanceof Piece)) return false;
 		Piece piece1 = (Piece) o;
-		return Objects.equals(piece, piece1.piece);
+		return Objects.equals(color, piece1.color) &&
+				Objects.equals(piece, piece1.piece);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(piece);
+		return Objects.hash(color, piece);
 	}
 
-	public Piece makePiece(String piece) {
-		return new Piece(piece);
+	@Override
+	public String toString() {
+		return "Piece{" +
+				"piece='" + piece + '\'' +
+				'}';
 	}
+
+
 }
--- chess/test/com/paractice/PieceTest.java
+++ chess/test/com/paractice/PieceTest.java
@@ -7,11 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PieceTest {
 	private static Logger logger = LoggerFactory.getLogger(PieceTest.class);
-	Piece piece;
+	private Piece piece;
 
 	@BeforeEach
 	public void setUp() {
@@ -21,18 +22,157 @@ public class PieceTest {
 	@Test
 	public void test_blank() {
 		Piece blank = piece.makeBlank();
-		assertEquals(blank, piece.makePiece(" "));
+		assertEquals(" ", blank.getPiece());
 	}
 
 	@Test
 	public void test_blank_exception() {
 		assertThrows(WrongPieceException.class,
-				() -> piece.makePiece("!"), "이거 어디에 나오는데");
+				() -> piece.makePiece("!"));
 	}
 
 
 	@Test
-	public void test_whitePawn() {
+	public void testMakeWhitePawn() {
+		Piece whitePawn = piece.makeWhitePawn();
+		assertEquals("p", whitePawn.getPiece());
+	}
 
+	@Test
+	public void testMakeWhitePawnException() {
+		assertThrows(WrongPieceException.class,
+				() -> piece.makeWhitePawn("P"));
+	}
+
+	@Test
+	public void testMakeWhiteRook() {
+		Piece whiteRook = piece.makeWhiteRook();
+		assertEquals("r", whiteRook.getPiece());
+	}
+
+	@Test
+	public void testMakeWhiteRookException() {
+		assertThrows(WrongPieceException.class,
+				() -> piece.makeWhiteRook("R"));
+	}
+
+	@Test
+	public void testMakeWhiteKnight() {
+		Piece whiteKnight = piece.makeWhiteKnight();
+		assertEquals("n", whiteKnight.getPiece());
+	}
+
+	@Test
+	public void testMakeWhiteKnightException() {
+		assertThrows(WrongPieceException.class,
+				() -> piece.makeWhiteKnight("N"));
+	}
+
+	@Test
+	public void testMakeWhiteBishop() {
+		Piece whiteBishop = piece.makeWhiteBishop();
+		assertEquals("b", whiteBishop.getPiece());
+	}
+
+	@Test
+	public void testMakeWhiteBishopException() {
+		assertThrows(WrongPieceException.class,
+				() -> piece.makeWhiteBishop("B"));
+	}
+
+	@Test
+	public void testMakeWhiteQueen() {
+		Piece whiteBishop = piece.makeWhiteQueen();
+		assertEquals("q", whiteBishop.getPiece());
+	}
+
+	@Test
+	public void testMakeWhiteQueenException() {
+		assertThrows(WrongPieceException.class,
+				() -> piece.makeWhiteQueen("Q"));
+	}
+
+	@Test
+	public void testMakeWhiteKing() {
+		Piece whiteBishop = piece.makeWhiteKing();
+		assertEquals("k", whiteBishop.getPiece());
+	}
+
+	@Test
+	public void testMakeWhiteKingException() {
+		assertThrows(WrongPieceException.class,
+				() -> piece.makeWhiteKing("K"));
+	}
+
+	@Test
+	public void testMakeBlackPawn() {
+		Piece blackPawn = piece.makeBlackPawn();
+		assertEquals("P", blackPawn.getPiece());
+	}
+
+	@Test
+	public void testMakeBlackPawnException() {
+		assertThrows(WrongPieceException.class,
+				() -> piece.makeBlackPawn("p"));
+	}
+
+	@Test
+	public void testMakeBlackRook() {
+		Piece blackRook = piece.makeBlackRook();
+		assertEquals("R", blackRook.getPiece());
+	}
+
+	@Test
+	public void testMakeBlackRookException() {
+		assertThrows(WrongPieceException.class,
+				() -> piece.makeBlackRook("r"));
+	}
+
+	@Test
+	public void testMakeBlackKnight() {
+		Piece whiteKnight = piece.makeBlackKnight();
+		assertEquals("N", whiteKnight.getPiece());
+	}
+
+	@Test
+	public void testMakeBlackKnightException() {
+		assertThrows(WrongPieceException.class,
+				() -> piece.makeBlackKnight("N"));
+	}
+
+	@Test
+	public void testMakeBlackBishop() {
+		Piece whiteBishop = piece.makeBlackBishop();
+		assertEquals("B", whiteBishop.getPiece());
+	}
+
+	@Test
+	public void testMakeBlackBishopException() {
+		assertThrows(WrongPieceException.class,
+				() -> piece.makeBlackBishop("b"));
+	}
+
+	@Test
+	public void testMakeBlackQueen() {
+		Piece whiteBishop = piece.makeBlackQueen();
+		assertEquals("Q", whiteBishop.getPiece());
+	}
+
+	@Test
+	public void testMakeBlackQueenException() {
+		assertThrows(WrongPieceException.class,
+				() -> piece.makeBlackQueen("q"));
+	}
+
+	@Test
+	public void testMakeBlackKing() {
+		Piece whiteBishop = piece.makeBlackKing();
+		assertEquals("K", whiteBishop.getPiece());
+	}
+
+	@Test
+	public void testMakeBlackKingException() {
+		assertThrows(WrongPieceException.class,
+				() -> piece.makeBlackKing("k"));
 	}
 }
